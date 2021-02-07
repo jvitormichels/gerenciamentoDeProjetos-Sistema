8b2df41278466a8dbe2f3aaf375ade774dcc1bda
jvitormichels <jvitormichels@gmail.com>
2021-02-06 22:08:54 -0300

    fix:  activities from a project with sequential ids, unique per project

--- phpFunctions/Activity_Submit.php
+++ phpFunctions/Activity_Submit.php
@@ -11,8 +11,12 @@
     $date_end = $_POST['date_end'];
     $finished = $_POST['finished'];
 
-    $sql = "INSERT INTO activities (activity_id, project_id, activity_name, date_start, date_end, finished) VALUES (NULL, '$project_id', '$activity_name', '$date_start', '$date_end', '$finished')";
-    
+    $activities = mysqli_query($link, "SELECT MAX(activity_id) AS largest_id FROM activities WHERE project_id=$project_id");
+    $row = mysqli_fetch_array($activities);
+    $activity_id = $row['largest_id'] + 1;
+
+    $sql = "INSERT INTO activities (activity_id, project_id, activity_name, date_start, date_end, finished) VALUES ($activity_id, '$project_id', '$activity_name', '$date_start', '$date_end', '$finished')";
+
     //valida a inserção
     //retire o redirecionamento para monitorar isso
     if ($link->query($sql) === TRUE) {
